4351fe0935782a8364a720c2520d5d73adc55d1a
yaotingho <andysmailg@gmail.com>
2024-09-10 00:02:09 +0800

    [Andys] add Frens Preview page

--- Assets/Scripts/Game/UI/Stolen/UIStolenInfo.cs
+++ Assets/Scripts/Game/UI/Stolen/UIStolenInfo.cs
@@ -7,7 +7,22 @@ using Simemes.Treasures;
 
 namespace Simemes.UI
 {
-    public class StolenInfo : MonoBehaviour
+    public class TreasureData
+    {
+        public float RemainTime;
+        public bool HasBuff;
+    }
+
+    public class PlayerData
+    {
+        public int Sprite;
+        public int Name;
+        public int Titles;
+        public System.DateTime LastUpdate;
+        public List<TreasureData> Treasures = new List<TreasureData>();
+    }
+
+    public class UIStolenInfo : MonoBehaviour
     {
         [SerializeField] private UIChestPanel _chestPanel;
         [SerializeField] private Text _name;
@@ -19,6 +34,7 @@ namespace Simemes.UI
         [SerializeField] private Button _stealBtn;
         [SerializeField] private GameObject _popupFrame;
         [SerializeField] private Text _popupText;
+        [SerializeField] private UIStolenView _stolenView;
 
         // for local Test ======================================================
         [SerializeField] private Sprite[] _images;
@@ -68,23 +84,13 @@ namespace Simemes.UI
             "President"
         };
 
-        public class TreasureData
-        {
-            public float RemainTime;
-            public bool HasBuff;
-        }
-
-        public class PlayerData
-        {
-            public int Sprite;
-            public int Name;
-            public int Titles;
-            public System.DateTime LastUpdate;
-            public List<TreasureData> Treasures = new List<TreasureData>();
-        }
 
         private List<PlayerData> _playerRecord = new List<PlayerData>();
         // ===============================================================
+        private PlayerData _playerData;
+        string _lastName;
+        string _lastTitle;
+        Sprite _lastSprite;
 
         private int _chestCount;
         private int _playerDataIndex = 0;
@@ -104,46 +110,51 @@ namespace Simemes.UI
 
         public void Steal()
         {
-            if (_chestPanel.TryGetEmptySlot(out UIChestSlot empty))
-            {
-                bool success;
-                bool hasBuff;
+            _stolenView.LoadInfo(_playerData, _lastName, _lastTitle, _lastSprite);
+            _stolenView.gameObject.SetActive(true);
+        }
 
-                int index = Random.Range(0, _chestCount);
+        public void Steal(int index)
+        {
+            //if (_chestPanel.TryGetEmptySlot(out UIChestSlot empty))
+            //{
+            //    bool success;
+            //    bool hasBuff;
+
+            //    //int index = Random.Range(0, _chestCount);
                 UIChestSlot slot = _slots[index];
-                hasBuff = slot.Content.HasBuff;
-                success = hasBuff ? false : Random.Range(0, 100) < 75;
+            //    hasBuff = slot.Content.HasBuff;
+            //    success = hasBuff ? false : Random.Range(0, 100) < 75;
 
-                if (hasBuff)
-                {
-                    _stealBtn.interactable = false;
-                }
+            //    if (hasBuff)
+            //    {
+            //        _stealBtn.interactable = false;
+            //    }
 
-                if (success)
-                {
+            //    if (success)
+            //    {
                     slot.gameObject.SetActive(false);
 
-                    // remove treasure data
-                    PlayerData playerData = _playerRecord[_playerDataIndex];
-                    playerData.Treasures.RemoveAt(index);
+            //        // remove treasure data
+            //        PlayerData playerData = _playerRecord[_playerDataIndex];
+            //        playerData.Treasures.RemoveAt(index);
 
-                    if (_chestPanel != null)
-                    {
-                        _chestPanel.AddChest(_chestPanel.Slots.IndexOf(empty));
+            //        if (_chestPanel != null)
+            //        {
+            //            _chestPanel.AddChest(_chestPanel.Slots.IndexOf(empty));
 
-                        empty.SetBox(slot.Content);
-                        empty.Seal();
-                    }
-                }
-                _popupText.text = hasBuff ? "Trigger guard!\n<color=red>Steal failed...</color>" : success ? "Steal succeeded!" : "<color=red>Steal failed...</color>"; ;// "偷取失敗\n觸發防護罩" : success ? "成功偷取" : "偷取失敗";
-            }
-            else
-            {
-                _popupText.text = "Your chest is full";//"你的寶箱已滿";
-            }
+            //            empty.SetBox(slot.Content);
+            //            empty.Seal();
+            //        }
+            //    }
+            //    _popupText.text = hasBuff ? "Trigger guard!\n<color=red>Steal failed...</color>" : success ? "Steal succeeded!" : "<color=red>Steal failed...</color>"; ;// "偷取失敗\n觸發防護罩" : success ? "成功偷取" : "偷取失敗";
+            //}
+            //else
+            //{
+            //    _popupText.text = "Your chest is full";//"你的寶箱已滿";
+            //}
 
-
-            _popupFrame.SetActive(true);
+            //_popupFrame.SetActive(true);
         }
 
         public void LoadNextInfo()
@@ -224,35 +235,35 @@ namespace Simemes.UI
         {
             // for local Test ======================================================
             int nameIndex = Random.Range(0, _names.Length);
-            string name = _names[nameIndex];
-            string title = _titles[nameIndex];
-            int spriteIndex = nameIndex < _images.Length ? nameIndex : _images.Length - 1;
-            Sprite sprite = _images[spriteIndex];
+            _lastName = _names[nameIndex];
+            _lastTitle = _titles[nameIndex];
+            int spriteIndex = nameIndex % _images.Length;// < _images.Length ? nameIndex : _images.Length - 1;
+            _lastSprite = _images[spriteIndex];
             LoadChestData();
 
             // log player data
-            PlayerData playerData = new PlayerData() { Name = nameIndex, Titles = nameIndex, Sprite = spriteIndex, LastUpdate = System.DateTime.Now };
+            _playerData = new PlayerData() { Name = nameIndex, Titles = nameIndex, Sprite = spriteIndex, LastUpdate = System.DateTime.Now };
             for (int i = 0; i < _chestCount; ++i)
             {
                 UIChestSlot slot = _slots[i];
-                playerData.Treasures.Add(new TreasureData() { RemainTime = slot.Content.RemainTime, HasBuff = slot.Content.HasBuff });
+                _playerData.Treasures.Add(new TreasureData() { RemainTime = slot.Content.RemainTime, HasBuff = slot.Content.HasBuff });
             }
-            _playerRecord.Add(playerData);
+            _playerRecord.Add(_playerData);
             _playerDataIndex = _playerRecord.Count - 1;
 
             // ===============================================================
 
-            Set(name, title, sprite);
+            Set(_lastName, _lastTitle, _lastSprite);
         }
 
         private void LoadInfoBase(int index)
         {
-            PlayerData playerData = _playerRecord[index];
-            string name = _names[playerData.Name];
-            string title = _titles[playerData.Titles];
-            Sprite sprite = _images[playerData.Sprite];
-            LoadChestData(playerData);
-            Set(name, title, sprite);
+            _playerData = _playerRecord[index];
+            _lastName = _names[_playerData.Name];
+            _lastTitle = _titles[_playerData.Titles];
+            _lastSprite = _images[_playerData.Sprite];
+            LoadChestData(_playerData);
+            Set(_lastName, _lastTitle, _lastSprite);
         }
     }
 }
---
+++ Assets/Scripts/Game/UI/Stolen/UIStolenView.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using Simemes.Treasures;
+
+namespace Simemes.UI
+{
+    public class UIStolenView : MonoBehaviour
+    {
+        [SerializeField] private UIChestPanel _chestPanel;
+        [SerializeField] private Text _name;
+        [SerializeField] private Text _title;
+        [SerializeField] private List<UIChestSlot> _slots;
+
+        [SerializeField] private UIStolenInfo _stolenInfo;
+        [SerializeField] private Button _stealBtn;
+        [SerializeField] private GameObject _popupFrame;
+        [SerializeField] private Text _popupText;
+        [SerializeField] private Image _character;
+
+        private int _chestCount;
+        private PlayerData _playerData;
+
+        private void Set(string name, string title)
+        {
+            _name.text = name;
+            _title.text = title;
+        }
+
+        public void Steal(int index)
+        {
+            if (_chestPanel.TryGetEmptySlot(out UIChestSlot empty))
+            {
+                bool success;
+                bool hasBuff;
+
+                UIChestSlot slot = _slots[index];
+                hasBuff = slot.Content.HasBuff;
+                success = hasBuff ? false : Random.Range(0, 100) < 75;
+
+                //if (hasBuff)
+                //    _stealBtn.interactable = false;
+
+                if (success)
+                {
+                    slot.gameObject.SetActive(false);
+
+                    // remove treasure data
+                    _playerData.Treasures.RemoveAt(index);
+
+                    if (_chestPanel != null)
+                    {
+                        _chestPanel.AddChest(_chestPanel.Slots.IndexOf(empty));
+
+                        empty.SetBox(slot.Content);
+                        empty.Seal();
+                    }
+                    _stolenInfo.Steal(index);
+                }
+                _popupText.text = hasBuff ? "Trigger guard!\n<color=red>Steal failed...</color>" : success ? "Steal succeeded!" : "<color=red>Steal failed...</color>"; ;// "偷取失敗\n觸發防護罩" : success ? "成功偷取" : "偷取失敗";
+            }
+            else
+            {
+                _popupText.text = "Your chest is full";//"你的寶箱已滿";
+            }
+
+            _popupFrame.SetActive(true);
+        }
+
+        public void LoadInfo(PlayerData playerData, string name, string title, Sprite sprite)
+        {
+            LoadInfoBase(playerData, name, title, sprite);
+        }
+
+        //private void LoadChestData()
+        //{
+        //    _chestCount = Random.Range(1, _slots.Count);
+        //    for (int i = 0; i < _slots.Count; ++i)
+        //    {
+        //        bool enable = i < _chestCount;
+        //        UIChestSlot slot = _slots[i];
+        //        slot.gameObject.SetActive(enable);
+        //        if (enable)
+        //        {
+        //            var treasureBoxConfig = TreasureSystem.instance.GetTreasureBoxConfig(0);
+        //            if (treasureBoxConfig == null)
+        //                return;
+
+        //            var treasureBox = new TreasureBox(treasureBoxConfig);
+        //            treasureBox.RemainTime = Random.Range(1000, 86400);
+
+        //            slot.SetBox(treasureBox);
+
+        //            if (Random.Range(0, 100) < 50)
+        //                slot.AddBuff(TreasureSystem.instance.GetBuff(1));
+
+        //            slot.Seal();
+        //        }
+        //    }
+        //}
+
+        private void LoadChestData(PlayerData playerData)
+        {
+            _playerData = playerData;
+            System.DateTime now = System.DateTime.Now;
+            _chestCount = playerData.Treasures.Count;
+            for (int i = 0; i < _slots.Count; ++i)
+            {
+                bool enable = i < _chestCount;
+                UIChestSlot slot = _slots[i];
+                slot.gameObject.SetActive(enable);
+                if (enable)
+                {
+                    var treasureBoxConfig = TreasureSystem.instance.GetTreasureBoxConfig(0);
+                    if (treasureBoxConfig == null)
+                        return;
+
+                    TreasureData treasureData = playerData.Treasures[i];
+                    var treasureBox = new TreasureBox(treasureBoxConfig);
+                    treasureBox.RemainTime = (float)(treasureData.RemainTime - (now - playerData.LastUpdate).TotalSeconds);
+
+                    slot.SetBox(treasureBox);
+
+                    if (treasureData.HasBuff)
+                        slot.AddBuff(TreasureSystem.instance.GetBuff(1));
+
+                    slot.Seal();
+                }
+            }
+        }
+
+        private void LoadInfoBase(PlayerData playerData, string name, string title, Sprite sprite)
+        {
+            LoadChestData(playerData);
+            Set(name, title);
+            _character.sprite = sprite;
+        }
+    }
+}
